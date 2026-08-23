18dced20653db2f04b4a0e54ce63fda10dfca823
manuelbernalcarvajal <119059748+manuelbernalcarvajal@users.noreply.github.com>
2026-08-23 17:10:32 +0200

    Update AdminSettings with new theme and mode values

--- lib/Settings/AdminSettings.php
+++ lib/Settings/AdminSettings.php
@@ -2,9 +2,9 @@
 
 namespace OCA\PwaSuite\Settings;
 
+use OCP\AppFramework\Http\TemplateResponse;
 use OCP\Settings\ISettings;
 use OCP\IConfig;
-use OCP\AppFramework\Http\TemplateResponse;
 
 class AdminSettings implements ISettings {
 
@@ -17,16 +17,19 @@ class AdminSettings implements ISettings {
     public function getForm(): TemplateResponse {
         $parameters = [
             'appName' => $this->config->getAppValue('pwa_suite', 'app_name', 'Nextcloud PWA'),
-            'themeColor' => $this->config->getAppValue('pwa_suite', 'theme_color', '#181818'),
+            'themeColor' => $this->config->getAppValue('pwa_suite', 'theme_color', '#0082c9'),
             'bgColor' => $this->config->getAppValue('pwa_suite', 'bg_color', '#181818'),
             'displayMode' => $this->config->getAppValue('pwa_suite', 'display_mode', 'standalone'),
+            'advancedMode' => $this->config->getAppValue('pwa_suite', 'advanced_mode', 'no'),
+            'customManifest' => $this->config->getAppValue('pwa_suite', 'custom_manifest', ''),
+            'customSw' => $this->config->getAppValue('pwa_suite', 'custom_sw', ''),
         ];
 
         return new TemplateResponse('pwa_suite', 'admin', $parameters, '');
     }
 
     public function getSection(): string {
-        return 'security'; // Pestaña de Seguridad en el panel de admin
+        return 'admin';
     }
 
     public function getPriority(): int {
