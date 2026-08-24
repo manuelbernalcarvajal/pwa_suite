82a0c7433615045b48d94acf3be4468155889363
manuelbernalcarvajal <119059748+manuelbernalcarvajal@users.noreply.github.com>
2026-08-24 02:25:45 +0200

    Refactor AdminController to use JSONResponse

--- lib/Controller/AdminController.php
+++ lib/Controller/AdminController.php
@@ -3,40 +3,66 @@
 namespace OCA\PwaSuite\Controller;
 
 use OCP\AppFramework\Controller;
-use OCP\AppFramework\Http\DataResponse;
-use OCP\AppFramework\Http\Attribute\AdminRequired;
-use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
+use OCP\AppFramework\Http\JSONResponse;
 use OCP\IRequest;
 use OCP\IConfig;
+use OCP\Files\IAppData;
 
 class AdminController extends Controller {
 
     private IConfig $config;
+    private IAppData $appData;
 
-    public function __construct(string $appName, IRequest $request, IConfig $config) {
+    public function __construct(string $appName, IRequest $request, IConfig $config, IAppData $appData) {
         parent::__construct($appName, $request);
         $this->config = $config;
+        $this->appData = $appData;
     }
 
-    #[AdminRequired]
-    #[NoCSRFRequired]
-    public function saveConfig(
-        string $appName,
-        string $themeColor,
-        string $bgColor,
-        string $displayMode,
-        string $advancedMode = 'no',
-        string $customManifest = '',
-        string $customSw = ''
-    ): DataResponse {
-        $this->config->setAppValue('pwa_suite', 'app_name', $appName);
-        $this->config->setAppValue('pwa_suite', 'theme_color', $themeColor);
-        $this->config->setAppValue('pwa_suite', 'bg_color', $bgColor);
-        $this->config->setAppValue('pwa_suite', 'display_mode', $displayMode);
-        $this->config->setAppValue('pwa_suite', 'advanced_mode', $advancedMode);
-        $this->config->setAppValue('pwa_suite', 'custom_manifest', $customManifest);
-        $this->config->setAppValue('pwa_suite', 'custom_sw', $customSw);
+    public function saveConfig(): JSONResponse {
+        $params = $this->request->getParams();
 
-        return new DataResponse(['status' => 'success', 'message' => 'Configuración PWA guardada']);
+        if (isset($params['appName'])) $this->config->setAppValue('pwa_suite', 'app_name', $params['appName']);
+        if (isset($params['themeColor'])) $this->config->setAppValue('pwa_suite', 'theme_color', $params['themeColor']);
+        if (isset($params['bgColor'])) $this->config->setAppValue('pwa_suite', 'bg_color', $params['bgColor']);
+        if (isset($params['displayMode'])) $this->config->setAppValue('pwa_suite', 'display_mode', $params['displayMode']);
+        if (isset($params['advancedMode'])) $this->config->setAppValue('pwa_suite', 'advanced_mode', $params['advancedMode']);
+        if (isset($params['customManifest'])) $this->config->setAppValue('pwa_suite', 'custom_manifest', $params['customManifest']);
+        if (isset($params['customSw'])) $this->config->setAppValue('pwa_suite', 'custom_sw', $params['customSw']);
+
+        return new JSONResponse(['status' => 'success']);
+    }
+
+    public function uploadIcon(): JSONResponse {
+        $uploaded = $this->request->getUploadedFile('pwa_icon');
+
+        if (!$uploaded || $uploaded['error'] !== UPLOAD_ERR_OK) {
+            return new JSONResponse(['status' => 'error', 'message' => 'Error al subir imagen'], 400);
+        }
+
+        $content = file_get_contents($uploaded['tmp_name']);
+        if (!$content) {
+            return new JSONResponse(['status' => 'error', 'message' => 'Archivo vacío'], 400);
+        }
+
+        try {
+            $folder = $this->appData->getFolder('icons');
+        } catch (\Exception $e) {
+            $folder = $this->appData->newFolder('icons');
+        }
+
+        try {
+            $file = $folder->getFile('app-icon.png');
+            $file->putContent($content);
+        } catch (\Exception $e) {
+            $file = $folder->newFile('app-icon.png');
+            $file->putContent($content);
+        }
+
+        $version = time();
+        $this->config->setAppValue('pwa_suite', 'has_custom_icon', 'yes');
+        $this->config->setAppValue('pwa_suite', 'icon_version', (string)$version);
+
+        return new JSONResponse(['status' => 'success', 'version' => $version]);
     }
 }
