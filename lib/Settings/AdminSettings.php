83abfd2d950f47082471860f342b64678db7b6d8
manuelbernalcarvajal <119059748+manuelbernalcarvajal@users.noreply.github.com>
2026-08-24 02:27:50 +0200

    Update theme color and add icon URL in AdminSettings

--- lib/Settings/AdminSettings.php
+++ lib/Settings/AdminSettings.php
@@ -18,14 +18,18 @@ class AdminSettings implements ISettings {
     public function getForm(): TemplateResponse {
         Util::addScript('pwa_suite', 'admin-script');
 
+        $iconVersion = $this->config->getAppValue('pwa_suite', 'icon_version', '1');
+        $iconUrl = Util::linkToRoute('pwa_suite.pwa.getIcon') . '?v=' . $iconVersion;
+
         $parameters = [
             'appName' => $this->config->getAppValue('pwa_suite', 'app_name', 'Nextcloud PWA'),
-            'themeColor' => $this->config->getAppValue('pwa_suite', 'theme_color', '#0082c9'),
+            'themeColor' => $this->config->getAppValue('pwa_suite', 'theme_color', '#181818'),
             'bgColor' => $this->config->getAppValue('pwa_suite', 'bg_color', '#181818'),
             'displayMode' => $this->config->getAppValue('pwa_suite', 'display_mode', 'standalone'),
             'advancedMode' => $this->config->getAppValue('pwa_suite', 'advanced_mode', 'no'),
             'customManifest' => $this->config->getAppValue('pwa_suite', 'custom_manifest', ''),
             'customSw' => $this->config->getAppValue('pwa_suite', 'custom_sw', ''),
+            'iconUrl' => $iconUrl,
         ];
 
         return new TemplateResponse('pwa_suite', 'admin', $parameters, '');
